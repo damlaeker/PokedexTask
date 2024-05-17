842a8055f9c930e77dc20e15c001d45d6310c7b1
Damla Eker <damlaeker@HolograsoftsAir.broadband>
2024-05-17 16:48:50 +0100

    first commit - added Alamofire

---
+++ PokedexTask/PokedexTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexTaskApp.swift
+//  PokedexTask
+//
+//  Created by Damla Eker on 17/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct PokedexTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
